67670c4383e52a064854327f3c48469b79d1f766
Dan <57096365+danrfletcher@users.noreply.github.com>
2023-12-31 16:46:32 +0000

    Creates data access logic for topic repository

--- Repositories/TopicRepository.cs
+++ Repositories/TopicRepository.cs
@@ -1,6 +1,27 @@
-﻿namespace be_fletchs_news_csharp.Repositories
+﻿using be_fletchs_news_csharp.Data;
+using be_fletchs_news_csharp.Models;
+using be_fletchs_news_csharp.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace be_fletchs_news_csharp.Repositories
 {
-    public class TopicRepository
+    public class TopicRepository : ITopicRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public TopicRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Topic>> GetAllTopicsAsync()
+        {
+            return await _context.Topics.ToListAsync();
+        }
+
+        public async Task<Topic?> GetTopicBySlugAsync(string slug)
+        {
+            return await _context.Topics.FirstOrDefaultAsync(t => t.Slug == slug);
+        }
     }
 }
