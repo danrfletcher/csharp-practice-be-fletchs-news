dff07d67188e265d7c2ab28118348e33583e9a87
Dan <57096365+danrfletcher@users.noreply.github.com>
2023-12-31 18:40:09 +0000

    Adds swagger docs descriptions to topics controller

--- Controllers/TopicsController.cs
+++ Controllers/TopicsController.cs
@@ -2,23 +2,51 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using be_fletchs_news_csharp.Models;
+using be_fletchs_news_csharp.Repositories.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace be_fletchs_news_csharp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TopicsController : Controller
+    public class TopicsController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
-        public TopicsController(ApplicationDbContext context)
+        private readonly ITopicRepository _topicRepository;
+
+        public TopicsController(ITopicRepository topicRepository)
         {
-            _context = context;
+            _topicRepository = topicRepository;
         }
 
+        /// <summary>
+        /// Retrieves all topics.
+        /// </summary>
+        /// <returns>A list of topics</returns>
+        /// <response code="200">Returns the list of topics</response>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Topic>>> GetTopics()
         {
-            return await _context.Topics.ToListAsync();
+            var topics = await _topicRepository.GetAllTopicsAsync();
+            return Ok(topics);
+        }
+
+        /// <summary>
+        /// Retrieves a specific topic by slug.
+        /// </summary>
+        /// <param name="slug">The slug of the topic to retrieve</param>
+        /// <returns>The requested topic</returns>
+        /// <response code="200">Returns the requested topic</response>
+        /// <response code="404">If the topic is not found</response>
+        [HttpGet("{slug}")]
+        public async Task<ActionResult<Topic>> GetTopicBySlug(string slug)
+        {
+            var topic = await _topicRepository.GetTopicBySlugAsync(slug);
+            if (topic == null)
+            {
+                return NotFound();
+            }
+            return Ok(topic);
         }
     }
 }
