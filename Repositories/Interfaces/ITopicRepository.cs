0eb51afa0eb90f9eaf474ea954e6b58371382a4f
Dan <57096365+danrfletcher@users.noreply.github.com>
2023-12-31 18:41:08 +0000

    Makes topic nullable value in topic model interface

--- Repositories/Interfaces/ITopicRepository.cs
+++ Repositories/Interfaces/ITopicRepository.cs
@@ -5,6 +5,6 @@ namespace be_fletchs_news_csharp.Repositories.Interfaces
     public interface ITopicRepository
     {
         Task<IEnumerable<Topic>> GetAllTopicsAsync();
-        Task<Topic> GetTopicBySlugAsync(string slug);
+        Task<Topic?> GetTopicBySlugAsync(string slug);
     }
 }
