936b2d786fd0d4c61da0fabc4184c16097e97a97
Dan <57096365+danrfletcher@users.noreply.github.com>
2023-12-31 18:39:48 +0000

    Adds config to program.cs to generate swagger docs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 
 using be_fletchs_news_csharp.Data;
+using be_fletchs_news_csharp.Repositories.Interfaces;
+using be_fletchs_news_csharp.Repositories;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace be_fletchs_news_csharp
 {
@@ -11,11 +14,20 @@ namespace be_fletchs_news_csharp
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
+
+            // Register repositories
+            builder.Services.AddScoped<ITopicRepository, TopicRepository>();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
 
             //Configure database connection here.
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
