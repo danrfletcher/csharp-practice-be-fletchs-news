ea34559290d3ce7fde4d5b5a23aeba3f23a771a3
Dan <57096365+danrfletcher@users.noreply.github.com>
2023-12-31 15:55:30 +0000

    creates topics controller

---
+++ Controllers/TopicsController.cs
@@ -0,0 +1,23 @@
+﻿using be_fletchs_news_csharp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace be_fletchs_news_csharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TopicsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public TopicsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Topic>>> GetTopics()
+        {
+            return await _context.Topics.ToListAsync();
+        }
+    }
+}
