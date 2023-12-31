0a7a4f3af06d0a9d2520bc87b836e1053e1d65e6
Dan <57096365+danrfletcher@users.noreply.github.com>
2023-12-31 16:46:16 +0000

    Changes namespace for applicationdbcontext (now it is in data folder)

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -2,14 +2,13 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using be_fletchs_news_csharp.Models;
 
-namespace be_fletchs_news_csharp
+namespace be_fletchs_news_csharp.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
-
         public DbSet<Article> Articles { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Topic> Topics { get; set; }
